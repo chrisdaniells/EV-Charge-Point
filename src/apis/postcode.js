e23117316936837487ef6cc3e62712cc270e2188
Chris Daniells <chrisdaniells@gmail.com>
2021-04-04 08:22:48 +0100

    add fetchPostcodeCoords function to postcode API service

--- src/apis/postcode.js
+++ src/apis/postcode.js
@@ -2,4 +2,18 @@ export default class PostcodeApi {
     constructor() {
         this.apiUrl = 'https://api.postcodes.io/postcodes/';
     }
+
+    fetchPostcodeCoords = (postcode = '') => {
+        return fetch(this.apiUrl + postcode)
+            .then((response) => response.json())
+            .then((json) => {
+                return {
+                    longitude: json.longitude,
+                    latitude: json.latitude
+                };
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 }
