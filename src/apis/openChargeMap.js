1f5b1a948ba5c19c63818e3a0ea82edf0890708a
Chris Daniells <chrisdaniells@gmail.com>
2021-04-04 08:18:22 +0100

    Set up classes for API services

---
+++ src/apis/openChargeMap.js
@@ -0,0 +1,6 @@
+export default class OpenChargeMapApi {
+    constructor() {
+        this.apiUrl = 'https://api.openchargemap.io/v3/';
+        this.apiKey = '';
+    }
+}
---
+++ src/apis/postcode.js
@@ -0,0 +1,5 @@
+export default class PostcodeApi {
+    constructor() {
+        this.apiUrl = 'https://api.postcodes.io/postcodes/';
+    }
+}
